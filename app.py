88a8a57513acc7896cdca649b000757a03de64c2
santyago-pixel <santyago@gmail.com>
2025-09-30 11:29:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1491,24 +1491,20 @@ try:
                         <span class="blue-text">Seguir todas las cotizaciones en TradingView</span>
                     </a>
                 </div>
-                <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
+                <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
                 {
-                "autosize": false,
+                "symbol": "IMV",
                 "width": "100%",
                 "height": "300",
-                "symbol": "IMV",
-                "interval": "D",
-                "timezone": "America/Argentina/Buenos_Aires",
-                "theme": "light",
-                "style": "1",
                 "locale": "es",
-                "toolbar_bg": "transparent",
-                "enable_publishing": false,
-                "hide_top_toolbar": true,
-                "hide_legend": false,
-                "save_image": false,
-                "container_id": "tradingview_widget",
-                "backgroundColor": "transparent"
+                "dateRange": "12M",
+                "colorTheme": "light",
+                "isTransparent": true,
+                "autosize": false,
+                "largeChartUrl": "",
+                "hideTopToolbar": true,
+                "hideLegend": false,
+                "saveImage": false
                 }
                 </script>
             </div>
